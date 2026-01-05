448259311c17d707d1f5b4796ee1423d5b61a021
김민규 <mkkim96@dosystem.kr>
2026-01-05 14:56:07 +0900

    feat: 동기부여 객체들 계층구조 데이터 클래스 추가

---
+++ sharedUI/src/commonMain/kotlin/dev/loki/ask/model/HierarchicalModel.kt
@@ -0,0 +1,18 @@
+package dev.loki.ask.model
+
+import dev.loki.domain.type.Category
+import kotlinx.datetime.LocalDate
+
+/**
+ * 계층 구조를 나타내는 공통 인터페이스
+ * @param Child 자식 요소의 타입. 최하위 레벨의 경우 Nothing 사용
+ */
+sealed interface HierarchicalModel<out Child> {
+    val id: Int
+    val title: String
+    val category: Category
+    val registeredDate: LocalDate
+    val endDate: LocalDate
+    val children: List<Child>
+    val receiveNotification: Boolean
+}
