afc19289086e0df639fdc49b2bdcf60488a1074c
김민규 <mkkim96@dosystem.kr>
2026-01-05 14:56:40 +0900

    feat: Quote(명언) 데이터 클래스 추가

---
+++ domain/src/commonMain/kotlin/dev/loki/domain/model/Quote.kt
@@ -0,0 +1,7 @@
+package dev.loki.domain.model
+
+data class Quote(
+    val id: Int,
+    val title: String,
+    val author: String,
+)
---
+++ sharedUI/src/commonMain/kotlin/dev/loki/ask/model/QuoteModel.kt
@@ -0,0 +1,6 @@
+package dev.loki.ask.model
+
+data class QuoteModel(
+    val title: String = "Success is the sum of small efforts, repeated day in and day out.",
+    val author: String = "Robert Collier",
+)
